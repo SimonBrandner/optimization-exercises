10aabab7b18d0c948724d1196b08d42ea6b068d3
Šimon Brandner <simon.bra.ag@gmail.com>
2026-04-01 09:01:27 +0200

    Do not print
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- code/newton.m
+++ code/newton.m
@@ -4,6 +4,6 @@ function approximation = newton(f, f_derivative, approximation, delta_max)
 		h  = f_derivative(approximation) \ f(approximation);
 		new_approximation = approximation - h;
 		delta = norm(approximation - new_approximation);
-		approximation = new_approximation
+		approximation = new_approximation;
 	end
 end
